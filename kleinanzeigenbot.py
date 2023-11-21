6e6946ea4778904c5ceccfc0f72fe7265d70baa6
Niko Tepe <niko.tepe@gmail.com>
2023-11-21 16:42:39 +0100

    basic working setup

--- kleinanzeigenbot.py
+++ kleinanzeigenbot.py
@@ -1,141 +1,139 @@
-import asyncio
+import threading
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 import os.path
-import requests
 import json
 import time
-import sys
 
-#header für req
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+class KleinanzeigenBot(threading.Thread):
 
-#erstelle timestamp im format: [Wed Jan  6 15:44:14 2021]
-def timestamp():
-    return '[' + time.asctime(time.localtime()) + '] '
+    #header für req
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
 
-def extract_list_item_data(item):
-    article = item.find('article', class_='aditem')
+    #erstelle timestamp im format: [Wed Jan  6 15:44:14 2021]
+    def timestamp(self):
+        return '[' + time.asctime(time.localtime()) + '] '
 
-    if article is None:
-        return None  
-    
-    data = {
-        'data-adid': article['data-adid'] if 'data-adid' in article.attrs else '',
-        'link': article['data-href'] if 'data-href' in article.attrs else '',
-        'image': item.find('img')['src'] if item.find('img') else '',
-        'location': item.select_one('.aditem-main--top--left').get_text(strip=True) if item.select_one('.aditem-main--top--left') else '',
-        'posted_time': item.select_one('.aditem-main--top--right').get_text(strip=True) if item.select_one('.aditem-main--top--right') else '',
-        'title': item.select_one('h2 a').get_text(strip=True) if item.select_one('h2 a') else '',
-        'description': item.select_one('.aditem-main--middle--description').get_text(strip=True) if item.select_one('.aditem-main--middle--description') else '',
-        'price': item.select_one('.aditem-main--middle--price-shipping--price').get_text(strip=True) if item.select_one('.aditem-main--middle--price-shipping--price') else '',
-        'shipping': item.select_one('.aditem-main--middle--price-shipping--shipping').get_text(strip=True) if item.select_one('.aditem-main--middle--price-shipping--shipping') else ''
-    }
-    return data
+    def extract_list_item_data(self, item):
+        article = item.find('article', class_='aditem')
 
-def return_items_from_req(searchterm):
+        if article is None:
+            return None  
+        
+        data = {
+            'data-adid': article['data-adid'] if 'data-adid' in article.attrs else '',
+            'link': article['data-href'] if 'data-href' in article.attrs else '',
+            'image': item.find('img')['src'] if item.find('img') else '',
+            'location': item.select_one('.aditem-main--top--left').get_text(strip=True) if item.select_one('.aditem-main--top--left') else '',
+            'posted_time': item.select_one('.aditem-main--top--right').get_text(strip=True) if item.select_one('.aditem-main--top--right') else '',
+            'title': item.select_one('h2 a').get_text(strip=True) if item.select_one('h2 a') else '',
+            'description': item.select_one('.aditem-main--middle--description').get_text(strip=True) if item.select_one('.aditem-main--middle--description') else '',
+            'price': item.select_one('.aditem-main--middle--price-shipping--price').get_text(strip=True) if item.select_one('.aditem-main--middle--price-shipping--price') else '',
+            'shipping': item.select_one('.aditem-main--middle--price-shipping--shipping').get_text(strip=True) if item.select_one('.aditem-main--middle--price-shipping--shipping') else ''
+        }
+        return data
 
-    # URL und Header für Webrequest
-    req_url = "https://www.kleinanzeigen.de/s-" + searchterm + "/k0"
+    def return_items_from_req(self, searchterm):
 
-    # Webrequest
-    print(timestamp() + 'sending web_request for term: \'' + searchterm + '\'')
-    req = Request(url=req_url, headers=headers) 
-    html = urlopen(req).read()
-    
-    # HTMLparsing
-    data =  BeautifulSoup(html, "html.parser" ).encode('UTF-8')
-    soup = BeautifulSoup(data, features="html.parser")
+        # URL und Header für Webrequest
+        req_url = "https://www.kleinanzeigen.de/s-" + searchterm + "/k0"
 
-    # finden der artikelliste in html
-    result = soup.find('ul', {'id':'srchrslt-adtable', 'class':'itemlist ad-list it3'})
+        # Webrequest
+        print(self.timestamp() + 'sending web_request for term: \'' + searchterm + '\'')
+        req = Request(url=req_url, headers=self.headers) 
+        html = urlopen(req).read()
+        
+        # HTMLparsing
+        data =  BeautifulSoup(html, "html.parser" ).encode('UTF-8')
+        soup = BeautifulSoup(data, features="html.parser")
 
-    #setze leere itemliste
-    item_list_html = ''
+        # finden der artikelliste in html
+        result = soup.find('ul', {'id':'srchrslt-adtable', 'class':'itemlist ad-list it3'})
 
-    #Error Handling if HTML parse empty
-    try:
-        item_list_html = result.find_all('li', class_="ad-listitem")
-
-    #wenn ein error auftritt handle diesen
-    except Exception as e:
-        print(timestamp() + 'Error Exception: ' + str(e))
+        #setze leere itemliste
         item_list_html = ''
 
-    #gebe die itemliste zurück
-    return item_list_html
-
-
-def append_items_to_json_file(item_list, filepath) :
-    with open(filepath, 'r') as file:
-        existing_items = json.load(file)
-
-    existing_adids = set(item['data-adid'] for item in existing_items)
-
-    # Initialisiere einen Zähler für die hinzugefügten Items
-    added_count = 0
-
-    for item in item_list:
-        item_data = extract_list_item_data(item)
-        # Überprüfen, ob item_data ein Dictionary ist und 'data-adid' enthält
-        if item_data and 'data-adid' in item_data and item_data['data-adid'] not in existing_adids:
-            existing_items.append(item_data)
-            existing_adids.add(item_data['data-adid'])
-            added_count += 1  # Erhöhe den Zähler
-
-    with open(filepath, 'w') as file:
-        json.dump(existing_items, file, indent=4)
-
-    return added_count
-
-
-
-def main():
-
-    # sys.argv enthält den Skriptnamen und die übergebenen Argumente
-    script_name = sys.argv[0]
-    args = sys.argv[1:]  # Alle Argumente nach dem Skriptnamen
-
-    stop_running = False
-    
-    searchterm = args[0]
-    sleeptime = int(args[1])
-    # min_price = args[3]
-    # max_price = args[4]
-
-    print("starting worker with searchterm " + searchterm + " and sleeptime " + str(sleeptime))
-
-    # Erstelle den Dateipfad
-    filepath = os.path.join(os.path.dirname(__file__), 'data', searchterm + '.json')
-
-    # Wenn der Ordner nicht existiert, erstelle diesen
-    directory = os.path.dirname(filepath)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    # Wenn die JSON-Datei nicht existiert, dann erstelle eine leere Datei
-    if not os.path.isfile(filepath):
-        with open(filepath, "w") as text_file:
-            text_file.write("[]")
-
-    while not stop_running:
+        #Error Handling if HTML parse empty
         try:
+            item_list_html = result.find_all('li', class_="ad-listitem")
 
-            # hole aktuelle items
-            item_list = return_items_from_req(searchterm)
-
-            added_items_count = append_items_to_json_file(item_list, filepath)
-
-            print(timestamp() + "added " + str(added_items_count) + " items")
-
-            #rollback to prev list
-            if item_list == '':
-                raise Exception("Fehler beim request aufgetreten: item_list ist leer")
-                
+        #wenn ein error auftritt handle diesen
         except Exception as e:
-            print(timestamp() + 'Error Exception: ' + str(e))
+            print(self.timestamp() + 'Error Exception: ' + str(e))
+            item_list_html = ''
 
-        # Asynchrones Warten
-        time.sleep(sleeptime)
-if __name__ == "__main__":
-    main()
+        #gebe die itemliste zurück
+        return item_list_html
+
+
+    def append_items_to_json_file(self, item_list, filepath) :
+        with open(filepath, 'r') as file:
+            existing_items = json.load(file)
+
+        existing_adids = set(item['data-adid'] for item in existing_items)
+
+        # Initialisiere einen Zähler für die hinzugefügten Items
+        added_count = 0
+
+        for item in item_list:
+            item_data = self.extract_list_item_data(item)
+            # Überprüfen, ob item_data ein Dictionary ist und 'data-adid' enthält
+            if item_data and 'data-adid' in item_data and item_data['data-adid'] not in existing_adids:
+                existing_items.append(item_data)
+                existing_adids.add(item_data['data-adid'])
+                added_count += 1  # Erhöhe den Zähler
+                # Hier fügen wir das neue Item in die message_bus_queue ein
+                self.message_bus_queue.put(item_data)
+
+        with open(filepath, 'w') as file:
+            json.dump(existing_items, file, indent=4)
+
+        return added_count
+
+
+
+    def __init__(self, searchterm, sleeptime, message_bus_queue):
+        threading.Thread.__init__(self)
+        self.searchterm = searchterm
+        self.sleeptime = sleeptime
+        self.message_bus_queue = message_bus_queue
+        self.stop_running = False
+
+
+        # Erstelle den Dateipfad
+        self.filepath = os.path.join(os.path.dirname(__file__), 'data', searchterm + '.json')
+
+        # Wenn der Ordner nicht existiert, erstelle diesen
+        directory = os.path.dirname(self.filepath)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        # Wenn die JSON-Datei nicht existiert, dann erstelle eine leere Datei
+        if not os.path.isfile(self.filepath):
+            with open(self.filepath, "w") as text_file:
+                text_file.write("[]")
+        ("search initialized")
+        self.run()
+
+    def run(self):
+        
+        print("starting search")
+        while True:
+            try:
+
+                # hole aktuelle items
+                item_list = self.return_items_from_req(self.searchterm)
+
+                added_items_count = self.append_items_to_json_file(item_list, self.filepath)
+
+                print(self.timestamp() + "added " + str(added_items_count) + " items")
+
+                #rollback to prev list
+                if item_list == '':
+                    raise Exception("Fehler beim request aufgetreten: item_list ist leer")
+                    
+
+            except Exception as e:
+                print(self.timestamp() + 'Error Exception: ' + str(e))
+
+            time.sleep(self.sleeptime)
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,11 +1,14 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ConversationHandler, ContextTypes
+import asyncio
+import threading
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes, filters, ConversationHandler, MessageHandler
 
 import logging
 
 import multiprocessing
 import subprocess
 import datetime
+import kleinanzeigenbot
 
 BOT_TOKEN = "6616611678:AAEVGAnVbc2w2biSU4JqvI9m-KA5hq_4pm8"
 
@@ -13,7 +16,9 @@ BOT_TOKEN = "6616611678:AAEVGAnVbc2w2biSU4JqvI9m-KA5hq_4pm8"
 SEARCH_TERM, SLEEP_TIME = range(2)
 
 worker_processes = []
-next_worker_id = 0
+current_worker_id = 0
+message_bus_queue = multiprocessing.Queue()
+chat_ids_for_notifications = set()
 
 
 # Konfigurieren Sie das Logging
@@ -27,6 +32,31 @@ def run_script_in_process(script_path, args):
     arg_list = ["python", script_path] + args
     subprocess.run(arg_list)
 
+def run_async_monitor(app):
+    # Create a new event loop for the thread
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    # Run the coroutine in the event loop
+    loop.run_until_complete(monitor_queue_and_notify(app))
+    loop.close()
+
+async def monitor_queue_and_notify(app):
+    while True:
+        item = message_bus_queue.get()  # Blockiert, bis ein Element verfügbar ist
+        for chat_id in chat_ids_for_notifications:
+            try:
+                # Erstellen Sie eine formatierte Nachricht
+                message_text = (
+                    f"{item['title']}\n"
+                    f"Preis: {item['price']} | {item['location']}\n"
+                    f"Versandmglk.: {item['shipping']}\n"
+                    f"Link: https://www.kleinanzeigen.de{item['link']}"
+                )
+
+                await app.bot.send_message(chat_id, message_text)
+            except Exception as e:
+                logging.error(f"Fehler beim Senden der Nachricht: {e}")
 
 async def start_worker(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     await update.message.reply_text("Wonach möchtest du suchen?")
@@ -35,20 +65,26 @@ async def start_worker(update: Update, context: ContextTypes.DEFAULT_TYPE) -> in
 async def search_term(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     user_input = update.message.text
     context.user_data['search_term'] = user_input.replace(' ', '-')
-    await update.message.reply_text("Wie oft möchtest du suchen (in Sekunden)?")
+    await update.message.reply_text("In welchem Intervall möchtest du suchen (in Sekunden)?")
     return SLEEP_TIME
 
 async def sleep_time(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:    
     user_input = update.message.text
-    context.user_data['sleep_time'] = user_input
+    # Speichern Sie die Schlafzeit als Integer im context.user_data
+    try:
+        context.user_data['sleep_time'] = int(user_input)
+    except ValueError:
+        await update.message.reply_text("Bitte gib eine gültige Zahl ein.")
+        return SLEEP_TIME
+
+    chat_id = update.effective_chat.id
+    chat_ids_for_notifications.add(chat_id)
+
 
     # Erstellen des Process-Objekts mithilfe der create_worker_process-Funktion
+    # Hier verwenden Sie die Werte aus context.user_data
     process, worker_info = create_worker_process(context.user_data['search_term'], context.user_data['sleep_time'])
-
-    # Prozess starten
-    process.start()
-
-    # Dictionary zur Liste hinzufügen
+    process.start()  # Starten Sie den Worker-Prozess
     worker_processes.append(worker_info)
 
     await update.message.reply_text("Worker gestartet mit den Einstellungen:\n"
@@ -59,50 +95,42 @@ async def sleep_time(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 
 # Funktion zur Erstellung eines Workers und eines Prozesses
 def create_worker_process(search_term, sleep_time):
+    global current_worker_id
 
-    # Verwenden Sie die nächste aufsteigende ID
-    global next_worker_id
-    worker_id = next_worker_id
-    next_worker_id += 1
-
-
-    process = multiprocessing.Process(
-        target=run_script_in_process, 
-        args=("kleinanzeigenbot.py", [search_term, sleep_time])
+    # Create the worker process
+    worker_process = multiprocessing.Process(
+        target=kleinanzeigenbot.KleinanzeigenBot,
+        args=(search_term, sleep_time, message_bus_queue)
     )
 
-    # Erstellen des Dictionary mit Metadaten, einschließlich der aufsteigenden ID
+    # Create the worker info dictionary
     worker_info = {
-        "process": process,
+        "process": worker_process,
         "search_term": search_term,
         "sleep_time": sleep_time,
         "start_time": datetime.datetime.now(),
-        "worker_id": worker_id  # Weisen Sie dem Worker eine aufsteigende ID zu
+        "worker_id": current_worker_id
     }
 
-    return process, worker_info
+    current_worker_id += 1
+
+    return worker_process, worker_info
 
 async def stop_worker(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if not context.args:
-        await update.message.reply_text("Bitte geben Sie die ID des zu stoppenden Workers an.")
-        return
 
-    worker_id_to_stop = int(context.args[0])  # Wandeln Sie die ID in eine Ganzzahl um
-    process_stopped = False
+    if(worker_processes):
+        keyboard = []
+        for worker_info in worker_processes:
+            # Jede Zeile in der Tastatur ist eine Liste von InlineKeyboardButtons
+            button_text = f"Worker ({worker_info['worker_id']})"
+            callback_data = f"stop_{worker_info['worker_id']}"
+            keyboard.append([InlineKeyboardButton(button_text, callback_data=callback_data)])
 
-    for worker_info in worker_processes:
-        if worker_info["worker_id"] == worker_id_to_stop:
-            worker_info["process"].terminate()
-            worker_info["process"].join(timeout=10)  # Warten Sie auf das Beenden des Prozesses für maximal 10 Sekunden
-            if not worker_info["process"].is_alive():
-                process_stopped = True
-                break
-
-    if process_stopped:
-        await update.message.reply_text(f"Worker mit ID '{worker_id_to_stop}' erfolgreich gestoppt.")
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await update.message.reply_text('Wähle einen Worker zum Stoppen:', reply_markup=reply_markup)
+    
     else:
-        await update.message.reply_text(f"Kein Worker mit ID '{worker_id_to_stop}' gefunden oder konnte nicht gestoppt werden.")
-
+        await update.message.reply_text('Es gibt derzeit keine aktiven Worker.')
 
 
 async def list_worker(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -116,25 +144,58 @@ async def list_worker(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
         sleep_time = worker_info["sleep_time"]
         start_time = worker_info["start_time"].strftime("%Y-%m-%d %H:%M:%S")
         worker_id = worker_info["worker_id"]  # Holen Sie die aufsteigende Worker-ID
-        worker_info_str += f"Worker {worker_id}:\n- Suchbegriff: {search_term}\n- Sleep Time: {sleep_time} Sekunden\n- Startzeit: {start_time}\n\n"
+        worker_info_str += f"Worker (ID: {worker_id}):\n- Suchbegriff: {search_term}\n- Sleep Time: {sleep_time} Sekunden\n- Startzeit: {start_time}\n\n"
 
     await update.message.reply_text(worker_info_str)
 
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # Erstellen der Inline-Tasten    
+    keyboard = [
+        [InlineKeyboardButton("Help-Option 1", callback_data='help_option_1'),
+        InlineKeyboardButton("Help-Option 2", callback_data='help_option_2')],
+        [InlineKeyboardButton("Help-Option 3", callback_data='help_option_3')]
+    ]
 
-    """Displays info on how to use the bot."""
+    # Erstellen der Markup
+    reply_markup = InlineKeyboardMarkup(keyboard)
 
-    await update.message.reply_text(
+    # Senden der Nachricht mit der Markup
+    await update.message.reply_text('Wähle eine Option aus der Liste unten:', reply_markup=reply_markup)
 
-        "to be implemented..."
+async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
 
-    )
+    data = query.data
+    # Überprüfen, ob die callback_data mit 'stop_' beginnt
+    if data.startswith("stop_"):
+        worker_id_to_stop = int(data.split("_")[1])
+        for worker_info in worker_processes:
+            if worker_info["worker_id"] == worker_id_to_stop:
+                worker_info["process"].terminate()
+                worker_processes.remove(worker_info)  # Remove from the list
+                await query.edit_message_text(f"Worker {worker_id_to_stop} gestoppt.")
+                return
+        await query.edit_message_text(f"Kein Worker mit der ID {worker_id_to_stop} gefunden.")
+
+    
+    if data.startswith("help_"):
+        # Callback-Daten verarbeiten und eine Antwort senden
+        if query.data == 'help_option_1':
+            await query.edit_message_text(text="Du hast Option 1 gewählt!")
+        elif query.data == 'help_option_2':
+            await query.edit_message_text(text="Du hast Option 2 gewählt!")
+        elif query.data == 'help_option_3':
+            await query.edit_message_text(text="Du hast Option 3 gewählt!")
 
 
 def main():
     # Erstellen Sie den Updater und übergeben Sie Ihr Bot-Token.
     app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+    # Starten Sie den Überwachungsthread
+    threading.Thread(target=run_async_monitor, args=(app,), daemon=True).start()
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('startworker', start_worker)],
@@ -146,18 +207,16 @@ def main():
     )
 
     # Definieren Sie CommandHandler
-    app.add_handler(CommandHandler("stopworker", stop_worker))
+    app.add_handler(CommandHandler("stopworker", stop_worker))    
     app.add_handler(CommandHandler("listworker", list_worker))
     app.add_handler(CommandHandler("help", help))
+    app.add_handler(CallbackQueryHandler(button_handler))
     app.add_handler(conv_handler)
         
     # Starten Sie den Bot
     app.run_polling()
     
 
-
-if __name__ == "__main__":
-    main()
 
 if __name__ == "__main__":
     main()
